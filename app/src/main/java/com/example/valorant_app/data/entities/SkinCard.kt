117b20d87d27a2c79c728ff2e887ba59a65f7c68
joaosehn2406 <jpssantosos5874@gmail.com>
2025-06-27 10:26:46 -0300

    feat: created SkinCard dataclass to deserialize JSON blocks

---
+++ app/src/main/java/com/example/valorant_app/data/entities/SkinCard.kt
@@ -0,0 +1,11 @@
+package com.example.valorant_app.data.entities
+
+import com.google.gson.annotations.SerializedName
+
+class SkinCard {
+    @SerializedName("displayName")
+    val displayName: String = ""
+
+    @SerializedName("displayIcon")
+    val displayIcon: String = ""
+}
