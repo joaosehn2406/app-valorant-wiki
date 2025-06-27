cfdcceda29df6d9de7d9c3be28c1b57c7e124797
joaosehn2406 <jpssantosos5874@gmail.com>
2025-06-27 11:09:55 -0300

    feat: created GunSingle class that contains the skin variations

---
+++ app/src/main/java/com/example/valorant_app/data/entities/GunSingle.kt
@@ -0,0 +1,63 @@
+package com.example.valorant_app.data.entities
+
+import com.google.gson.annotations.SerializedName
+
+data class GunSingle(
+    @SerializedName("displayName")
+    val displayName: String = "",
+
+    @SerializedName("defaultSkinUuid")
+    val defaultSkinUuid: String = "",
+
+    @SerializedName("skins")
+    val skins: List<Skin> = listOf()
+) {
+    data class Skin(
+        @SerializedName("uuid")
+        val uuid: String = "",
+
+        @SerializedName("displayName")
+        val displayName: String = "",
+
+        @SerializedName("themeUuid")
+        val themeUuid: String = "",
+
+        @SerializedName("contentTierUuid")
+        val contentTierUuid: String = "",
+
+        @SerializedName("displayIcon")
+        val displayIcon: String = "",
+
+        @SerializedName("wallpaper")
+        val wallpaper: String? = null,
+
+        @SerializedName("assetPath")
+        val assetPath: String = "",
+
+        @SerializedName("chromas")
+        val chromas: List<Chroma> = listOf()
+    )
+
+    data class Chroma(
+        @SerializedName("uuid")
+        val uuid: String = "",
+
+        @SerializedName("displayName")
+        val displayName: String = "",
+
+        @SerializedName("displayIcon")
+        val displayIcon: String = "",
+
+        @SerializedName("fullRender")
+        val fullRender: String = "",
+
+        @SerializedName("swatch")
+        val swatch: String? = null,
+
+        @SerializedName("streamedVideo")
+        val streamedVideo: String? = null,
+
+        @SerializedName("assetPath")
+        val assetPath: String = ""
+    )
+}
