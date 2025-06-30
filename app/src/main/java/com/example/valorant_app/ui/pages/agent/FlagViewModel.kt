38c452f5c6e5232aa733bc4a5130af2128c9f980
joaosehn2406 <jpssantosos5874@gmail.com>
2025-06-30 17:14:15 -0300

    feat: created FlagViewModel to communicate with UI

---
+++ app/src/main/java/com/example/valorant_app/ui/pages/agent/FlagViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.valorant_app.ui.pages.agent
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.valorant_app.data.repository.CountryFlagRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class FlagViewModel @Inject constructor(
+    private val countryRepo: CountryFlagRepository
+) : ViewModel() {
+
+    private val _flagUrl = MutableStateFlow<String?>(null)
+    val flagUrl: StateFlow<String?> = _flagUrl
+
+    private val _loading = MutableStateFlow(false)
+    val loading: StateFlow<Boolean> = _loading
+
+    private val _error = MutableStateFlow<String?>(null)
+    val error: StateFlow<String?> = _error
+
+    fun loadFlag(countryName: String) {
+        viewModelScope.launch {
+            _loading.value = true
+            countryRepo
+                .getCountryFlagsByName(countryName)
+                .firstOrNull()?.flags?.png
+                ?.let {
+                    _flagUrl.value = it
+                    _error.value = null
+                }
+                ?: run {
+                    _error.value = "País não encontrado"
+                }
+            _loading.value = false
+        }
+    }
+}
