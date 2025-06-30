407a2b789bac0c6a521a2413a379fc84bac6391d
joaosehn2406 <jpssantosos5874@gmail.com>
2025-06-30 16:44:15 -0300

    feat: created path to make the get request

--- app/src/main/java/com/example/valorant_app/data/entities/CountryFlags.kt
+++ app/src/main/java/com/example/valorant_app/data/entities/CountryFlags.kt
@@ -3,9 +3,14 @@ package com.example.valorant_app.data.entities
 import com.google.gson.annotations.SerializedName
 
 data class CountryResponse(
-    @SerializedName("flags") val flags: Flags
+    @SerializedName("flags")
+    val flags: Flags
 )
+
 data class Flags(
-    @SerializedName("png") val png: String,
-    @SerializedName("svg") val svg: String
+    @SerializedName("png")
+    val png: String,
+
+    @SerializedName("svg")
+    val svg: String
 )
--- app/src/main/java/com/example/valorant_app/data/services/ApiService.kt
+++ app/src/main/java/com/example/valorant_app/data/services/ApiService.kt
@@ -1,14 +1,19 @@
 package com.example.valorant_app.data.services
 
 import com.example.valorant_app.data.entities.AgentCard
+import com.example.valorant_app.data.entities.CountryResponse
 import com.example.valorant_app.data.entities.WeaponCard
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
     @GET("agents")
-    suspend fun getAllAgentsCard() : ApiResponse<List<AgentCard>>
+    suspend fun getAllAgentsCard(): ApiResponse<List<AgentCard>>
 
     @GET("weapon")
-    suspend fun getAllWeaponCard() : ApiResponse<List<WeaponCard>>
+    suspend fun getAllWeaponCard(): ApiResponse<List<WeaponCard>>
+
+    @GET("v3.1/name/{country}")
+    suspend fun getByName(@Path("country") countryName: String): List<CountryResponse>
 }
