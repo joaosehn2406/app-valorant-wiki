2b0f048e9b4c8fb8a1060b217b91cdfb734ef325
joaosehn2406 <jpssantosos5874@gmail.com>
2025-07-01 10:41:47 -0300

    feat: created SkinsScreenViewModel to use in UI

--- app/src/main/java/com/example/valorant_app/ui/pages/skin/SkinsScreenViewModel.kt
+++ app/src/main/java/com/example/valorant_app/ui/pages/skin/SkinsScreenViewModel.kt
@@ -1,13 +1,32 @@
 package com.example.valorant_app.ui.pages.skin
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.valorant_app.data.repository.WeaponRepository
+import com.example.valorant_app.ui.pages.agent.AgentUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SkinsScreenViewModel @Inject constructor(
     private val weaponRepository: WeaponRepository
 ) : ViewModel(){
+
+    private val _uiState = MutableStateFlow<WeaponUiState>(WeaponUiState.Loading)
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            val response = weaponRepository.getAllWeaponCard()
+            if (response.status == 200) {
+                _uiState.value = WeaponUiState.Success(response.data)
+            } else {
+                _uiState.value = WeaponUiState.Error("Erro: ${response.status}")
+            }
+        }
+    }
 
 }
