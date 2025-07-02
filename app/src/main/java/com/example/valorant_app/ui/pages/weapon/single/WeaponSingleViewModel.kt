300667ab8c265b7653c8db7b5a93168b797a972c
joaosehn2406 <jpssantosos5874@gmail.com>
2025-07-02 11:26:45 -0300

    feat: created WeaponSingleViewModel with fetchWeaponSingle method to fetch data

--- app/src/main/java/com/example/valorant_app/ui/pages/weapon/single/WeaponSingleViewModel.kt
+++ app/src/main/java/com/example/valorant_app/ui/pages/weapon/single/WeaponSingleViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.valorant_app.ui.pages.weapon.single
 
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.valorant_app.data.repository.WeaponRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,4 +14,23 @@ class WeaponSingleViewModel @Inject constructor(
     val weaponSingleRepository: WeaponRepository
 ) : ViewModel() {
 
+    private val _uiState = MutableStateFlow<WeaponSingleUiState>(WeaponSingleUiState.Loading)
+    private val uiState = _uiState.asStateFlow()
+
+    fun fetchWeaponSingle(weaponId: String) {
+        _uiState.value = WeaponSingleUiState.Loading
+
+        viewModelScope.launch {
+            val response = weaponSingleRepository.getWeaponById(weaponId)
+            try {
+                if (response.status == 200) {
+                    _uiState.value = WeaponSingleUiState.Success(response.data)
+                } else {
+                    _uiState.value = WeaponSingleUiState.Error("Erro: ${response.status}")
+                }
+            } catch (e: Exception) {
+                _uiState.value = WeaponSingleUiState.Error("Erro: ${response.status}")
+            }
+        }
+    }
 }
